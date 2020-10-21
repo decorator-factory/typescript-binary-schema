757e2bc646390affd4799822dd96be0d646cf607
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2020-10-21 21:35:12 +0300

    move library code to schema/

--- common.ts
+++
@@ -1,4 +0,0 @@
-export function absurd<A>(): A {
-    const f = (): any => null;
-    return f();
-}
--- test/lib.spec.ts
+++ test/lib.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import * as Lib from '../lib';
+import * as Lib from '../schema';
 import 'mocha';
 
 describe("Hello, world!", () => {
